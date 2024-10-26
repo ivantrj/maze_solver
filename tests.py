e4a6f5957efd4feef58d558305ee4ee72d40e790
Ivan Trajanovski <ivantrajanovski@gmail.com>
2024-10-26 18:47:31 +0200

    finish project

--- tests.py
+++ tests.py
@@ -29,6 +29,20 @@ class Tests(unittest.TestCase):
             False,
         )
 
+    def test_reset_cells_visited(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            m1._cells[0][0].visited,
+            False,
+        )
+        m1._reset_cells_visited()
+        self.assertEqual(
+            m1._cells[0][0].visited,
+            False,
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
